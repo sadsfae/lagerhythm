494f0400469c55cbd6e793bad0d83e5dad95f8fb
Will Foster <wfoster@redhat.com>
2013-03-22 13:40:51 +0100

    Adding Perl script and example CGI

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 pivo-tocene
 ===========
 
-pivo-tocene
+This is a simple Perl script which calls the twitter::lite
+API and captures text via CGI then passes it along to twitter.
+
+In order to utilize this you'd need to create a developer account
+for twitter and then obtain the various secret keys needed.
+
+This was originally hosted on Red Hat's 'Open Shift'
+(http://www.openshift.com) and used to track the amount
+of draft beer (pivo tocene) I consumed while in the Czech Republic
+on work assignment.
+
---
+++ pivo.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+
+use CGI;
+
+# Create the CGI object
+my $query = new CGI;
+
+# Output the HTTP header
+print $query->header ( );
+
+# Capture form results
+my $comments = $query->param("comments");
+
+# Filter form results
+$comments = filter_field ( $comments );
+
+########### POST TO TWITTER URL ###########
+
+use Net::Twitter::Lite;
+
+my $nt = Net::Twitter::Lite->new(
+  consumer_key        => 'XXXXXXXXXXXXXXXXXXXXX',
+  consumer_secret     => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  access_token        => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  access_token_secret => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+);
+
+my $result = eval { $nt->update($comments) };
+
+warn "$@\n" if $@;
+
+########### AUTO GENERATE HTML PAGE WHEN DONE ##########
+
+print <<END_HTML;
+<html>
+<head></head>
+<body>You've recorded pivo tocene!<br>
+<br>Enjoy yourself, and have fun in life!</body>
+</html>
+END_HTML
+
+# Functions for filtering user input
+
+sub filter_field
+{
+  my $field = shift;
+  $field =~ s/From://gi;
+  $field =~ s/To://gi;
+  $field =~ s/BCC://gi;
+  $field =~ s/CC://gi;
+  $field =~ s/Subject://gi;
+  $field =~ s/Content-Type://gi;
+  return $field;
+}
+
+sub filter_header_field
+{
+  my $field = shift;
+  $field =~ s/From://gi;
+  $field =~ s/To://gi;
+  $field =~ s/BCC://gi;
+  $field =~ s/CC://gi;
+  $field =~ s/Subject://gi;
+  $field =~ s/Content-Type://gi;
+  $field =~ s/[\0\n\r\|\!\/\<\>\^\$\%\*\&]+/ /g;
+  return $field;
+}
